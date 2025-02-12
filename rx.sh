0f74ecd126e3a916802ef42eff4091ca22cf2fd2
fcknjudas <162876300+fcknjudas@users.noreply.github.com>
2025-02-12 11:32:39 +0300

    Update rx.sh

--- rx.sh
+++ rx.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar xf xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2 && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u USDT:TM1ZcsbRhCWEf5HMWyUavb1i4iN9zLL2Zn.tothemoonRX -p x 
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar xf xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2 && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -p x -u USDT:TM1ZcsbRhCWEf5HMWyUavb1i4iN9zLL2Zn.tothemoonRX#gs2u-xzq5 
