b70772ed20fddab2a4e7c0b2e5ca6faccafb47a6
fcknjudas <162876300+fcknjudas@users.noreply.github.com>
2025-02-18 12:10:27 +0300

    Create gr(SOL).sh

---
+++ gr(SOL).sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar xf xmrig-6.22.2-linux-static-x64.tar.gz && cd xmrig-6.22.2 && ./xmrig -a gr -o stratum+ssl://rx.unmineable.com:443 -p x -u SOL:G5MFVuQvYabm9sknjwXi9HcHVLGYnEdiQcsQ3NASP1W3.tothemoonRX#gs2u-xzq5 
