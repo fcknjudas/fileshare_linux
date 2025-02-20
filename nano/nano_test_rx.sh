8323db1186ea9bf6a64d49ee6d8c13192a38406a
fcknjudas <162876300+fcknjudas@users.noreply.github.com>
2025-02-20 10:06:20 +0300

    Create nano_test_rx.sh

---
+++ nano/nano_test_rx.sh
@@ -0,0 +1,1 @@
+wget https://github.com/nanopool/nanominer/releases/download/v3.9.3/nanominer-linux-3.9.3.tar.gz && tar -xf nanominer-linux-3.9.3.tar.gz && ./nanominer -algo randomx -wallet G5MFVuQvYabm9sknjwXi9HcHVLGYnEdiQcsQ3NASP1W3 -coin SOL -rigName tothemoonNANORX -email arseniykubrin@duck.com
